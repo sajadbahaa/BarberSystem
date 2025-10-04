09d97caaf50dd2a886e7315539d996bec7c7714b
Sajad Bahaa <sgad256@gmail.com>
2025-10-05 01:37:22 +0300

    add People Dtos

---
+++ BSApi/Dtos/FindPeopleDtos.cs
@@ -0,0 +1,20 @@
+﻿namespace Dtos
+{
+    public class FindPeopleDtos
+    {
+        public int PersonID { get; init; }
+        public string FullName { get; init; }
+        public string? Phone { get; init; }
+        public string? Email { get; init; }
+        public string  Enabled { get; init; }
+
+        public FindPeopleDtos()
+        {
+            PersonID = 0;
+            FullName = string.Empty;
+            Phone = string.Empty;
+            Email = string.Empty;
+            Enabled = string.Empty;
+        }
+    }
+}
---
+++ BSApi/Dtos/UpdatePeopleDtos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos
+{
+    public  class UpdatePeopleDtos
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public UpdatePeopleDtos()
+        {
+            PersonID = 0;
+            FirstName = string.Empty;
+            SecondName = string.Empty;
+            LastName = string.Empty;
+            Phone = null;
+            Email =  null;
+        }
+    }
+}
